819f50ceddbca41cb559fe0680aac1cba109eca7
kichu3000 <anandakrishnanvb3000@gmail.com>
2025-09-27 21:05:33 +0530

    Changed to spring boot and added user table

---
+++ src/main/java/airlinemanagement/model/User.java
@@ -0,0 +1,29 @@
+package airlinemanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private boolean isAdmin;
+}
